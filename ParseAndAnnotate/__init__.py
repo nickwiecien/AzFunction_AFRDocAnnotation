48c618516783d43d4f1e9976c2ab0f395ceb8c32
Nick Kwiecien <nikwieci@microsoft.com>
2022-11-29 20:08:29 -0600

    Migrating source code

---
+++ ParseAndAnnotate/__init__.py
@@ -0,0 +1,140 @@
+import logging
+
+import azure.functions as func
+import os
+from azure.storage.blob import ContainerClient
+import shutil
+import json
+from pdf_annotate import PdfAnnotator, Location, Appearance
+import tempfile
+from PyPDF2 import PdfWriter, PdfReader
+from pdf2image import convert_from_path
+import fitz
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    storage_account_name = os.getenv('STORAGE_ACCOUNT_NAME', 'afrdemostorage')
+    storage_account_key = os.getenv('STORAGE_ACCOUNT_KEY', 'YOUR-STORAGE-KEY')
+    storage_account_container = os.getenv('STORAGE_ACCOUNT_CONTAINER', 'testing')
+    annotation_container = os.getenv('ANNOTATION_CONTAINER', 'annotations')
+
+    data = json.loads(req.get_json()['data'])
+    # data = (req.get_json()['data'])
+    blob_name = req.get_json()['blob_name']
+
+    max_height = data["boundingRegions"][0]['polygon'][7]
+    max_width = data["boundingRegions"][0]['polygon'][2]
+
+    data = data['fields']
+
+    final_result = {}
+    final_result['FILENAME'] = storage_account_container + '/' + blob_name
+
+    container_client = ContainerClient(f'https://{storage_account_name}.blob.core.windows.net/', storage_account_container, credential=storage_account_key)
+    blob_client = container_client.get_blob_client(blob_name)
+    dir(blob_client)
+
+    tempdir = tempfile.gettempdir()
+    with open(f'{tempdir}/{blob_name}', 'wb') as file:
+        file.write(blob_client.download_blob().readall())
+
+    # UPDATE THIS LIST WITH THE NAMES OF EXPECTED FIELDS FROM YOUR CUSTOM AFR MODEL
+    expected_fields = ['REQUESTED_DELIVERY_DATE', 'ACTUAL_DELIVERY_DATE', 'TANK_NUMBER', 'END_GALLONS']
+
+    delivery_pdf = PdfAnnotator(f'{tempdir}/{blob_name}')
+
+    for field in expected_fields:
+        final_result[field] = ''
+        try:
+            final_result[field]=data[field]['valueString']
+            regions = data[field]['boundingRegions']
+            pts = []
+            for reg in regions:
+                page_num = reg['pageNumber']-1
+                page_size = delivery_pdf.get_size(page_num)
+                for i in range(0, len(reg['polygon']), 2):
+                    pts.append([(((reg['polygon'][i] * page_size[0])/max_width)),(page_size[1]-((reg['polygon'][i+1]*page_size[1])/max_height))])
+                delivery_pdf.add_annotation(
+                    'polygon',
+                    Location(points=pts, page=page_num),
+                    Appearance(stroke_color=(1, 0, 0, 1), stroke_width=2),
+                )
+        except Exception as e:
+            print(e)
+            pass
+        
+    delivery_pdf.write(f'{tempdir}/{blob_name}'.replace('.pdf', '_ANNOTATED.pdf'))
+    images = fitz.open(f'{tempdir}/{blob_name}'.replace('.pdf', '_ANNOTATED.pdf'))
+    dpi = 300  # choose desired dpi here
+    zoom = dpi / 72  # zoom factor, standard: 72 dpi
+    magnify = fitz.Matrix(zoom, zoom)  # magnifies in x, resp. y direction
+    for image in images:
+        hold = image.get_pixmap(matrix=magnify)
+        hold.save(f'{tempdir}/{blob_name}'.replace('.pdf', '_ANNOTATED.pdf').replace('.pdf', '.png'))
+
+    final_result['ANNOTATED_FILENAME'] = f'annotations/{blob_name}'.replace('.pdf', '_ANNOTATED.pdf').replace('.pdf', '.png')
+
+    reader = PdfReader(f'{tempdir}/{blob_name}'.replace('.pdf', '_ANNOTATED.pdf'))
+
+    for field in expected_fields:
+        final_result[field+'_ANNOTATION'] = ''
+        try:
+            final_result[field]=data[field]['valueString']
+            regions = data[field]['boundingRegions']
+            pts = []
+            for reg in regions:
+                page_num = reg['pageNumber']-1
+                page_size = delivery_pdf.get_size(page_num)
+                reader = PdfReader(f'{tempdir}/{blob_name}'.replace('.pdf', '_ANNOTATED.pdf'))
+                writer = PdfWriter()
+                page = reader.pages[page_num]
+                xs = []
+                ys = []
+                for i in range(len(reg['polygon'])):
+                    if i % 2:
+                        ys.append(reg['polygon'][i])
+                    else :
+                        xs.append(reg['polygon'][i])
+                min_x = (min(xs) * (page_size[0]/max_width))-125
+                max_x = (max(xs) * (page_size[0]/max_width))+125
+                min_y = page_size[1]-(min(ys) * (page_size[1]/max_height))-125
+                max_y = page_size[1]-(max(ys) * (page_size[1]/max_height))+125
+                
+                page.cropbox.upper_left = (max_x, max_y)
+                page.cropbox.lower_right = (min_x, min_y)
+                
+                writer.add_page(page)
+                with open(f'{tempdir}/{blob_name}'.replace('.pdf', f'_{field}.pdf').replace('.pdf', '_ANNOTATED.pdf'), 'wb') as file:
+                    writer.write(file)
+                
+                final_result[field+'_ANNOTATION'] = f'annotations/{blob_name}'.replace('.pdf', f'_{field}.pdf').replace('.pdf', '_ANNOTATED.pdf').replace('.pdf', '.png')
+                
+                images = fitz.open(f'{tempdir}/{blob_name}'.replace('.pdf', f'_{field}.pdf').replace('.pdf', '_ANNOTATED.pdf'))
+                dpi = 300  # choose desired dpi here
+                zoom = dpi / 72  # zoom factor, standard: 72 dpi
+                magnify = fitz.Matrix(zoom, zoom)  # magnifies in x, resp. y direction
+                for image in images:
+                    hold = image.get_pixmap(matrix=magnify)
+                    hold.save(f'{tempdir}/{blob_name}'.replace('.pdf', f'_{field}.pdf').replace('.pdf', '_ANNOTATED.pdf').replace('.pdf', '.png'))
+                    break
+
+                print(min_x)
+        except Exception as e:
+            print(e)
+            pass
+        
+
+    container_client = ContainerClient(f'https://{storage_account_name}.blob.core.windows.net/', annotation_container, credential=storage_account_key)
+
+    files = os.listdir(f'{tempdir}')
+    for file in files:
+        if '.png' in file:
+            data = open(f'{tempdir}/{file}', 'rb').read()
+            container_client.upload_blob(file, data, overwrite=True)
+
+    logging.info(final_result)
+
+    return func.HttpResponse(json.dumps(final_result), status_code=200)
+ 
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# AzFunction_AFRDocAnnotation
+# Azure Function for Parsing Form Recognizer Results + Annotating Documents
+
+Contained within this repo is sample code for an [Azure Function](https://learn.microsoft.com/en-us/azure/azure-functions/functions-overview) which parses response results from a custom Azure Form Recognizer model, and annotates images according to identified and extracted text from AFR.
+
+## Environment Setup
+Before running this project, create a `local.settings.json` file in the root directory. This file needs to have the following entries under the `values` section:
+
+| Key                                 | Value                                    |
+|-------------------------------------|------------------------------------------|
+| AzureWebJobsStorage                 | The connection string to the storage account used by the Functions runtime.  To use the storage emulator, set the value to UseDevelopmentStorage=true |
+| FUNCTIONS_WORKER_RUNTIME            | Set this value to `python` as this is a python Function App |
+| WOUND_STORAGE_CONNECTION_STRING | Connection string to a storage account that will be used to upload wound images (expected container name: `images`)  and export modified\annotated images (expected container name: `analysis`) |
+| STORAGE_ACCOUNT_NAME     | Name of an Azure Storage Account which holds PDF documents which have been parsed by Azure Form Recognizer |
+| STORAGE_ACCOUNT_KEY     | Key for Azure Storage Account referenced above |
+| DOCUMENT_CONTAINER_NAME     | Name of the blob container which contains the PDF documents which have been parsed by AFR |
+| ANNOTATION_CONTAINER_NAME     | Name of the blob container where annotated images should be saved (should be separate from the storage ) |
+
+## Modifying the Function
+The function can be modified by updating the code within the `./ParseAndAnnotate/init.py` file. 
+
+<b>NOTE: To run this function successfully you will need to modify line 44 in this file and add the fields which you expect to see in your results from your custom Form Recognizer model.</b>
+
+## Deployment
+Prior to deploying this function, you will need to provide an Azure Function App which can be done inside the Azure Portal or from VS Code by following the [instructions here](https://learn.microsoft.com/en-us/azure/azure-functions/create-first-function-vs-code-python?pivots=python-mode-decorators#publish-the-project-to-azure). We recommend deploying this function directly from VS Code - a guide for doing so [can be found here](https://learn.microsoft.com/en-us/azure/azure-functions/create-first-function-vs-code-python?pivots=python-mode-decorators#deploy-the-project-to-azure).
